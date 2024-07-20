1b65fced495cde0373f88d4f7e3c2581bb8964b4
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-20 12:39:21 +0200

    Dispatching a route

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -15,16 +15,19 @@ class App
 
   public function run()
   {
-    echo "Application is running!";
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    $this->router->dispatch($path, $method);
   }
 
-  public function get(string $path)
+  public function get(string $path, array $controller)
   {
-    $this->router->addRoute("GET", $path);
+    $this->router->addRoute("GET", $path, $controller);
   }
-
+  /*
   public function post(string $path)
   {
     $this->router->addRoute("POST", $path);
-  }
+  }*/
 }
