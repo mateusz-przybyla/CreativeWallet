4dc4353d95ff9bc3fac055401132a1b00904a6e9
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-20 18:00:14 +0200

    Instantiating a class with dependencies, using Reflective Programming

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,12 +4,73 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Framework\Exceptions\ContainerException;
+use ReflectionClass, ReflectionNamedType;
+
 class Container
 {
   private array $definitions = [];
+  private array $resolved = [];
 
   public function addDefinitions(array $newDefinitions)
   {
-    dd($newDefinitions);
+    $this->definitions = array_merge($this->definitions, $newDefinitions);
+  }
+
+  public function resolve(string $className)
+  {
+    $reflectionClass = new ReflectionClass($className);
+
+    if (!$reflectionClass->isInstantiable()) {
+      throw new ContainerException("Class {$className} is not instantiable");
+    }
+
+    $constructor = $reflectionClass->getConstructor();
+
+    if (!$constructor) {
+      return new $className;
+    }
+
+    $params = $constructor->getParameters();
+
+    if (count($params) === 0) {
+      return new $className;
+    }
+
+    $dependencies = [];
+
+    foreach ($params as $param) {
+      $name = $param->getName();
+      $type = $param->getType();
+
+      if (!$type) {
+        throw new ContainerException("Failed to resolve class {$className} because param {$name} is missing a type hint.");
+      }
+
+      if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+        throw new ContainerException("Failed to resolve class {$className} bacause invalid param name.");
+      }
+      $dependencies[] = $this->get($type->getName());
+    }
+
+    return $reflectionClass->newInstanceArgs($dependencies);
+  }
+
+  public function get(string $id)
+  {
+    if (!array_key_exists($id, $this->definitions)) {
+      throw new ContainerException("Class {$id} does not exist in container.");
+    }
+
+    if (array_key_exists($id, $this->resolved)) { //singleton pattern
+      return $this->resolved[$id];
+    }
+
+    $factory = $this->definitions[$id]; //factory pattern
+    $dependency = $factory($this);
+
+    $this->resolved[$id] = $dependency;
+
+    return $dependency;
   }
 }
