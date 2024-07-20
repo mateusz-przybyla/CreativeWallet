1531063c5d85375359727e1bbc27e82b3b5e17af
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-20 15:18:47 +0200

    Escaping data and autoloading routes

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\{HomeController};
+
+use function App\Config\registerRoutes;
 
 $app = new App();
 
-$app->get("/", [HomeController::class, 'home']);
-//$app->get("/about");
+registerRoutes($app);
 
 return $app;
