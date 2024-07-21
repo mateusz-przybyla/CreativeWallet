4e704dd77472c3e9d64cbe983951a5bfbf9f578d
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-21 12:41:43 +0200

    Creating a SessionMiddleware

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -7,11 +7,13 @@ namespace App\Config;
 use Framework\App;
 use App\Middlewares\{
   TemplateDataMiddleware,
-  ValidationExceptionMiddleware
+  ValidationExceptionMiddleware,
+  SessionMiddleware
 };
 
 function registerMiddleware(App $app)
 {
   $app->addMiddleware(TemplateDataMiddleware::class);
   $app->addMiddleware(ValidationExceptionMiddleware::class);
+  $app->addMiddleware(SessionMiddleware::class);
 }
