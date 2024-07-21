68790718f0a853bd1f2241c5b134a0d55d72ec83
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-21 18:29:23 +0200

    Using phpdotenv package

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace App;
 
-use Framework\TemplateEngine;
+use Framework\{
+  TemplateEngine,
+  Database
+};
 use App\Config\Paths;
 use App\Services\ValidatorService;
 
 return [
   TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
-  ValidatorService::class => fn () => new ValidatorService(Paths::VIEW)
+  ValidatorService::class => fn () => new ValidatorService(Paths::VIEW),
+  Database::class => fn () => new Database($_ENV['DB_DRIVER'], [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME']
+  ], $_ENV['DB_USER'], $_ENV['DB_PASS'])
 ];
