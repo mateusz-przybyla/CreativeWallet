ea321b4a997430aa173d0b5835ed74bba63367af
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-21 17:09:52 +0200

    Creating Database class - connection and query method

---
+++ cli.php
@@ -0,0 +1,39 @@
+<?php
+
+include __DIR__ . '/src/Framework/Database.php';
+
+use Framework\Database;
+
+$db = new Database(
+  'mysql',
+  [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'creativewallet'
+  ],
+  'root',
+  ''
+);
+
+echo "connected to database";
+
+/*
+require __DIR__ . '/vendor/autoload.php';
+
+use Framework\Database;
+use App\Config\Paths;
+use Dotenv\Dotenv;
+
+//$dotenv = Dotenv::createImmutable(Paths::ROOT);
+//$dotenv->load();
+
+$db = new Database($_ENV['DB_DRIVER'], [
+  'host' => $_ENV['DB_HOST'],
+  'port' => $_ENV['DB_PORT'],
+  'dbname' => $_ENV['DB_NAME']
+], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+
+$sqlFile = file_get_contents("./database.sql");
+
+$db->query($sqlFile);
+*/
